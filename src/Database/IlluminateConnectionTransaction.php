e2f33360210c8b1857c7af759c1ef88c258c2af1
Zaher Ghaibeh <zaher@zaher.dev>
2023-05-03 21:56:44 +0300

    Adding php-cs-fixer GitHub workflow

--- src/Database/IlluminateConnectionTransaction.php
+++ src/Database/IlluminateConnectionTransaction.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/**
+ * Copyright (c) 2023 Dragonfly Development Inc
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/dflydev/dflydev-eventsauce-support-for-laravel
+ */
+
 namespace Dflydev\EventSauce\SupportForLaravel\Database;
 
 use Dflydev\EventSauce\Support\Transaction\Transaction;
